86139bfafa2e2930f2d27d3609b320fffcc9a8ba
shwetank <shwetank@freecharge.com>
2018-03-12 23:44:04 +0530

    start up script for common programs

---
+++ startApp.sh
@@ -0,0 +1,10 @@
+/home/shwetank/Software/eclipse/eclipse -data /home/shwetank/workspace  &
+
+/home/shwetank/Software/eclipse/eclipse -data /home/shwetank/workspace1 &
+
+google-chrome &
+
+firefox &
+
+subl &
+
